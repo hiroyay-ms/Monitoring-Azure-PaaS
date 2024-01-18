f141a0d2f8007cef1972461017f20425b26237bd
Hiroya Yamamoto <hiroyay@microsoft.com>
2024-01-18 14:58:21 +0900

    update functions project

--- src/CS/Api/AppInsightsTelemetryInitializer.cs
+++ src/CS/Api/AppInsightsTelemetryInitializer.cs
@@ -5,9 +5,6 @@ public class AppInsightsTelemetryInitializer : ITelemetryInitializer
 {
     public void Initialize(ITelemetry telemetry)
     {
-        if (string.IsNullOrEmpty(telemetry.Context.Cloud.RoleName))
-        {
-            telemetry.Context.Cloud.RoleName = "backend-api";
-        }
+        telemetry.Context.Cloud.RoleName = "backend-api";
     }
 }
