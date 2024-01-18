5bbb3292b62af6cd5d5dc63673f37936174e565c
Hiroya Yamamoto <hiroyay@microsoft.com>
2024-01-18 15:09:58 +0900

    update functions project

--- src/CS/Api/AppInsightsTelemetryInitializer.cs
+++ src/CS/Api/AppInsightsTelemetryInitializer.cs
@@ -1,7 +1,7 @@
 using Microsoft.ApplicationInsights.Channel;
 using Microsoft.ApplicationInsights.Extensibility;
 
-public class AppInsightsTelemetryInitializer : ITelemetryInitializer
+public class CloudRoleNameTelemetryInitializer : ITelemetryInitializer
 {
     public void Initialize(ITelemetry telemetry)
     {
